f50ccbe3164b168fa6c8a46acf9a8b3ff3b361ac
NealC17 <41767471+NealC17@users.noreply.github.com>
2019-03-03 12:09:18 -0800

    ElectionResult constructor

--- src/ElectionResult.java
+++ src/ElectionResult.java
@@ -3,4 +3,15 @@ public class ElectionResult {
     private double demVotes, gopVotes, totalVotes, perGop, diff, perPointDiff;
     private String stateAbbr, countyName;
 
+    public ElectionResult(double demVotes, double gopVotes, double totalVotes, double perGop, double diff, double perPointDiff, String stateAbbr, String countyName) {
+        this.demVotes = demVotes;
+        this.gopVotes = gopVotes;
+        this.totalVotes = totalVotes;
+        this.perGop = perGop;
+        this.diff = diff;
+        this.perPointDiff = perPointDiff;
+        this.stateAbbr = stateAbbr;
+        this.countyName = countyName;
+    }
+    
 }
