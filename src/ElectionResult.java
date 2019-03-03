8e287f5670365e107465df7d1a1566127e2fce14
NealC17 <41767471+NealC17@users.noreply.github.com>
2019-03-03 12:11:08 -0800

    ElectionResult getters

--- src/ElectionResult.java
+++ src/ElectionResult.java
@@ -13,5 +13,36 @@ public class ElectionResult {
         this.stateAbbr = stateAbbr;
         this.countyName = countyName;
     }
-    
+
+    public double getDemVotes() {
+        return demVotes;
+    }
+
+    public double getGopVotes() {
+        return gopVotes;
+    }
+
+    public double getTotalVotes() {
+        return totalVotes;
+    }
+
+    public double getPerGop() {
+        return perGop;
+    }
+
+    public double getDiff() {
+        return diff;
+    }
+
+    public double getPerPointDiff() {
+        return perPointDiff;
+    }
+
+    public String getStateAbbr() {
+        return stateAbbr;
+    }
+
+    public String getCountyName() {
+        return countyName;
+    }
 }
